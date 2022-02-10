3bb2e9cfe9ba8da19dad585cb5ae9fed67ae8072
Maccio92 <valerio362@gmail.com>
2022-02-10 16:07:29 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+// A questo punto, iniziamo a prendere confidenza con le rotte e le views:
+// cancelliamo la view welcome.blade.php e creiamo una nostra homepage.
+// Facciamo quindi sì che la rotta / visualizzi home.blade.php
+// Inizialmente stampiamo un Hello World, poi passiamo dei dati alla view in modo da visualizzarli dinamicamente con Blade.
+
+Route::get('/', function () {
+    $data = [
+        'name' => 'Aldo',
+        'lastname' => 'Baglio',
+        'cittadinanza' => 'italiana',
+    ];
+    return view('home', $data);
+});
